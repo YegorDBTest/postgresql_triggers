7fdbfeaf708b21d6573026ab71b4be5e948a5f7f
Yegor Bitensky <yegorbitensky@gmail.com>
2020-09-24 00:56:26 +0300

    add joins diagrams

--- joins.sql
+++ joins.sql
@@ -23,16 +23,14 @@ INSERT INTO test_joins_2 (a, c) VALUES (4, 4);
 
 -- Inner join
 
-SELECT test_joins_1.b, test_joins_2.c
-    FROM test_joins_1, test_joins_2
-    WHERE test_joins_1.a = test_joins_2.a;
---  b | c 
--- ---+---
---  X | 1
---  X | 2
---  Y | 3
--- (3 rows)
-
+-- +--------+
+-- |        |
+-- |   +----|---+
+-- |   |####|   |
+-- |   |####|   |
+-- +---|----+   |
+--     |        |
+--     +--------+
 SELECT test_joins_1.b, test_joins_2.c
     FROM test_joins_1 INNER JOIN test_joins_2
     ON (test_joins_1.a = test_joins_2.a);
@@ -46,6 +44,14 @@ SELECT test_joins_1.b, test_joins_2.c
 
 -- Left outer join
 
+-- +--------+
+-- |########|
+-- |## +----|---+
+-- |###|####|   |
+-- |###|####|   |
+-- +---|----+   |
+--     |        |
+--     +--------+
 SELECT test_joins_1.b, test_joins_2.c
     FROM test_joins_1 LEFT OUTER JOIN test_joins_2
     ON (test_joins_1.a = test_joins_2.a);
@@ -57,6 +63,14 @@ SELECT test_joins_1.b, test_joins_2.c
 --  Z |  
 -- (4 rows)
 
+-- +--------+
+-- |########|
+-- |## +----|---+
+-- |###|    |   |
+-- |###|    |   |
+-- +---|----+   |
+--     |        |
+--     +--------+
 SELECT test_joins_1.b, test_joins_2.c
     FROM test_joins_1 LEFT OUTER JOIN test_joins_2
     ON (test_joins_1.a = test_joins_2.a)
@@ -69,6 +83,14 @@ SELECT test_joins_1.b, test_joins_2.c
 
 -- Right outer join
 
+-- +--------+
+-- |        |
+-- |   +----|---+
+-- |   |####|###|
+-- |   |####|###|
+-- +---|----+ ##|
+--     |########|
+--     +--------+
 SELECT test_joins_1.b, test_joins_2.c
     FROM test_joins_1 RIGHT OUTER JOIN test_joins_2
     ON (test_joins_1.a = test_joins_2.a);
@@ -80,6 +102,14 @@ SELECT test_joins_1.b, test_joins_2.c
 --    | 4
 -- (4 rows)
 
+-- +--------+
+-- |        |
+-- |   +----|---+
+-- |   |    |###|
+-- |   |    |###|
+-- +---|----+ ##|
+--     |########|
+--     +--------+
 SELECT test_joins_1.b, test_joins_2.c
     FROM test_joins_1 RIGHT OUTER JOIN test_joins_2
     ON (test_joins_1.a = test_joins_2.a)
@@ -92,6 +122,14 @@ SELECT test_joins_1.b, test_joins_2.c
 
 -- Full outer join
 
+-- +--------+
+-- |########|
+-- |## +----|---+
+-- |###|####|###|
+-- |###|####|###|
+-- +---|----+ ##|
+--     |########|
+--     +--------+
 SELECT test_joins_1.b, test_joins_2.c
     FROM test_joins_1 FULL OUTER JOIN test_joins_2
     ON (test_joins_1.a = test_joins_2.a);
@@ -104,6 +142,14 @@ SELECT test_joins_1.b, test_joins_2.c
 --    | 4
 -- (5 rows)
 
+-- +--------+
+-- |########|
+-- |## +----|---+
+-- |###|    |###|
+-- |###|    |###|
+-- +---|----+ ##|
+--     |########|
+--     +--------+
 SELECT test_joins_1.b, test_joins_2.c
     FROM test_joins_1 FULL OUTER JOIN test_joins_2
     ON (test_joins_1.a = test_joins_2.a)
