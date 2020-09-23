bd409615493ff6ea6ac62f1168a2441ed4ad58dd
Yegor Bitensky <yegorbitensky@gmail.com>
2020-09-24 01:12:37 +0300

    triggers additions

--- triggers.sql
+++ triggers.sql
@@ -1,8 +1,8 @@
-CREATE TABLE test1 (
+CREATE TABLE test_triggers_1 (
     data integer
 );
 
-CREATE TABLE test2 (
+CREATE TABLE test_triggers_2 (
     data integer
 );
 
@@ -19,10 +19,10 @@ CREATE TABLE hashes_holder (
 
 CREATE OR REPLACE FUNCTION add_test_timestamp() RETURNS TRIGGER AS $$
     BEGIN
-        IF EXISTS(SELECT * FROM timestamps_holder WHERE table_name = TG_ARGV[0]) THEN
-            UPDATE timestamps_holder SET dt = now() WHERE table_name = TG_ARGV[0];
+        IF EXISTS(SELECT * FROM timestamps_holder WHERE table_name = TG_TABLE_NAME) THEN
+            UPDATE timestamps_holder SET dt = now() WHERE table_name = TG_TABLE_NAME;
         ELSE
-            INSERT INTO timestamps_holder SELECT TG_ARGV[0], now();
+            INSERT INTO timestamps_holder SELECT TG_TABLE_NAME, now();
         END IF;
         RETURN NULL;
     END;
@@ -32,10 +32,10 @@ CREATE OR REPLACE FUNCTION add_test_hash() RETURNS TRIGGER AS $$
     DECLARE
         hash_value text := md5(random()::text);
     BEGIN
-        IF EXISTS(SELECT * FROM hashes_holder WHERE table_name = TG_ARGV[0]) THEN
-            UPDATE hashes_holder SET hash = hash_value WHERE table_name = TG_ARGV[0];
+        IF EXISTS(SELECT * FROM hashes_holder WHERE table_name = TG_TABLE_NAME) THEN
+            UPDATE hashes_holder SET hash = hash_value WHERE table_name = TG_TABLE_NAME;
         ELSE
-            INSERT INTO hashes_holder SELECT TG_ARGV[0], hash_value;
+            INSERT INTO hashes_holder SELECT TG_TABLE_NAME, hash_value;
         END IF;
         RETURN NULL;
     END;
@@ -43,21 +43,21 @@ $$ LANGUAGE plpgsql;
 
 
 CREATE TRIGGER add_test1_timestamp
-    AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE ON test1
+    AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE ON test_triggers_1
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE add_test_timestamp('test1');
+    EXECUTE PROCEDURE add_test_timestamp();
 
 CREATE TRIGGER add_test2_timestamp
-    AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE ON test2
+    AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE ON test_triggers_2
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE add_test_timestamp('test2');
+    EXECUTE PROCEDURE add_test_timestamp();
 
 CREATE TRIGGER add_test1_hash
-    AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE ON test1
+    AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE ON test_triggers_1
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE add_test_hash('test1');
+    EXECUTE PROCEDURE add_test_hash();
 
 CREATE TRIGGER add_test2_hash
-    AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE ON test2
+    AFTER INSERT OR UPDATE OR DELETE OR TRUNCATE ON test_triggers_2
     FOR EACH STATEMENT
-    EXECUTE PROCEDURE add_test_hash('test2');
+    EXECUTE PROCEDURE add_test_hash();
